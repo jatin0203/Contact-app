51de3fc260688d1f528902a97704bb90c8962d7f
jatin0203 <jatinkanyaljr@gmail.com>
2023-01-02 19:32:43 +0530

    modified app.js on 2nd Jan

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Hey how you doin...
+          Lets gear up
         </p>
         <h1>
           We are going to create a contact app...
