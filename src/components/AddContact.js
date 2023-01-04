aeea319ef044750568d5e2684ae46fcdee651fe1
jatin0203 <jatinkanyaljr@gmail.com>
2023-01-04 16:38:41 +0530

    done till adding reverse props

--- src/components/AddContact.js
+++ src/components/AddContact.js
@@ -1,18 +1,43 @@
 import React from 'react';
 
 class AddContact extends React.Component {
+    state={
+        name:"",
+        email:"",
+    }
+    add=(e)=>{
+        e.preventDefault();
+        if(this.state.name==="" && this.state.email===""){
+            alert("All fields are mandatory");
+            return;
+        }
+        this.props.addContactHandler(this.state);
+        console.log(this.state);
+    };
     render(){
         return(
             <div className='ui main'>
                 <h2>Add Contact</h2>
-                <form className='ui form'>
+                <form className='ui form' onSubmit={this.add}>
                     <div className='field'>
                         <label>Name</label>
-                        <input type="text" name="name" placeholder='Enter the Name'></input>
+                        <input 
+                        type="text" 
+                        name="name" 
+                        placeholder='Enter the Name' 
+                        value={this.state.name}
+                        onChange={(e)=>{this.setState({name:e.target.value})}}
+                        ></input>
                     </div>
                     <div className='field'>
                         <label>Email</label>
-                        <input type="text" name="email" placeholder='Enter the email'></input>
+                        <input 
+                        type="text" 
+                        name="email" 
+                        placeholder='Enter the email'
+                        value={this.state.email}
+                        onChange={(e)=>{this.setState({email:e.target.value})}}
+                        ></input>
                     </div>
                     <button className='ui button blue'>Add</button>
                 </form>
--- src/components/App.js
+++ src/components/App.js
@@ -1,29 +1,20 @@
-import React from "react";
+import React,{useState} from "react";
 import './App.css';
 import Header from './Header';
 import ContactList from './ContactList';
 import AddContact from './AddContact';
 
 function App() {
-  const contacts=[
-    {
-      id:1,
-      name:"Dipesh",
-      email:"dipesh@contact.com"
-    },
-    {
-      id:2,
-      name:"Ramesh",
-      email:"rajesh@contact.com"
-    },
-  ];
+  const [contacts,setContacts]=useState([]);
+  const addContactHandler=(contact)=>{
+    console.log(contact);
+  };
   return(
     <div className="ui container">
       <Header/>
-      <AddContact/>
+      <AddContact addContactHandler={addContactHandler}/>
       <ContactList contacts={contacts}/>
     </div>
   );
 }
-
 export default App;
